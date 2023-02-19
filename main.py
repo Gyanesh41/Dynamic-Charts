ef40c8dc2db4caf1e273d44245e8700e0ddd4596
Gyanesh Raj <93124096+Gyanesh41@users.noreply.github.com>
2023-02-19 22:14:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,168 @@
 import matplotlib.pyplot as plt;
+import numpy as np;
 import pandas as pd;
 import seaborn as sns;
-def plot(charttype="line",library="Pyplot",
-         data=None,x=None,y=None,colors=None,annot=False,bins=None,
-         textprops=None,explode=None,xlabels=None,ylabels=None,vmin=-100,vmax=100,edgecolor="black",
-         hue=None,title=None,xtick=None,ytick=None,rotate_x=None,rotate_y=None,xlim=None,ylim=None,
-         labels=None,autopct=None,save="fig.png"):
-    ctrl=0
-    if library=="Pyplot" or library=="pyplot":
-    
-        if charttype=="pie":
-            plt.pie(x=x, explode=explode,labels=labels,colors=colors,textprops=textprops,autopct=autopct)
-            ctrl=ctrl+1
 
-        elif charttype=="line":
-            plt.plot(x,y,colors)
+argdict={'charttype':"line",'library':"Matplotlib",        
+         'data':None,'x':None,'y':None,'hue':None,'color':None,
+         'title':None,'xtick':None,'ytick':None,
+         'rotate_x':None,'rotate_y':None,'xlim':None,
+         'ylim':None,'savename':"C:/Users/rugyr/OneDrive/Pictures/fig.png"}
 
-        elif charttype=="bar":
-            plt.bar(data=data,x=x,height=y,color=colors)
-            
-        elif charttype=="hist":
-            plt.hist(data=data,x=x,color=colors,edgecolor=edgecolor,width=1,bins=bins,align="left")
-            
-        elif charttype=="scatter":
-            plt.scatter(x=x,y=y,c=colors)
-            
-        elif charttype=="heatmap":
-            plt.imshow(X=data)
-            cax = plt.axes([vmin,vmin+vmax/2,(vmin+vmax)/2,vmin/2 +vmax,vmax])
-            plt.colorbar(cax=cax)
+def dynamic_chart(argdict):
+    preinputs(argdict);
+    if argdict.get('library')=="seaborn":
+        charttype=input("[ 'heatmmap' , 'count' , 'bar' , 'hist' , 'scatter' ]\nPlease provide chart type : ")
+        argdict.update([('charttype',charttype)])
+        seaborn(argdict);
+        
+    elif argdict.get('library')=="matplotlib":
+        charttype=input("[ 'pie' , 'count' , 'bar' , 'hist' , 'scatter' ]\nPlease provide chart type : ")
+        argdict.update([('charttype',charttype)])
+        df=argdict.get("data")
+        matplotlib(argdict,df);
+    else: print("Error : Wrong Library");
+    addfeature=input("Do you want to use additional features [y/n] : ")
+    if addfeature=="y":
+        postinputs(argdict)
+    elif  addfeature=="n":print("")
+    else:
+        print("Error: Add. feature : Wrong input")
+        exit()
+    plt.title(argdict.get('title'))
+    plt.xticks(rotation=argdict.get('rotate_x'));
+    plt.yticks(rotation=argdict.get('rotate_y'));
+    plt.savefig(argdict.get("savename"));
+    plt.show();
     
-    elif library=="Seaborn" or library=="seaborn":
-        if charttype=="line":
-            sns.lineplot(data=data,x=x,y=y,c=colors,hue=hue)
-            
-        elif charttype=="bar":
-            sns.barplot(data=data,x=x,y=y,hue=hue,color=colors)
-        
-        elif charttype=="hist":
-            sns.histplot(data=data,x=x,y=y,hue=hue,color=colors)
-        
-        elif charttype=="heatmap":
-            sns.heatmap(data=data,vmin=vmin,vmax=vmax,annot=annot)
-            ctrl=ctrl+1
-        
-        elif charttype=="scatter":
-            sns.scatterplot(data=data,x=x,y=y,hue=hue,color=colors)
-            
-        elif charttype=="countplot":
-            sns.countplot(data=data,x=x,y=y,hue=hue,color=colors)
+def seaborn(argdict):
+    if argdict.get('charttype')=="scatter":
+        scattersns(argdict);
+    elif argdict.get('charttype')=="line":
+        linesns(argdict);
+    elif argdict.get('charttype')=="count":
+        countsns(argdict)
+    elif argdict.get('charttype')=="hist":
+        histsns(argdict)
+    elif argdict.get('charttype')=="bar":
+        barsns(argdict)
+    elif argdict.get('charttype')=="heatmap":
+        heatsns(argdict)
+    else:
+        print("Error : Wrong Charttype")
     
-    plt.title(title)
-    if ctrl==0:
-        plt.xlabel(xlabels)
-        plt.ylabel(ylabels)
-        plt.xticks(xtick,rotation=rotate_x)
-        plt.yticks(ytick,rotation=rotate_y)
-        plt.xlim(xlim)
-        plt.ylim(ylim)
-    plt.savefig(save)
-    plt.show()
+def matplotlib(argdict,df):
+    if argdict.get('charttype')=="pie":
+        piemat(argdict,df);
+    elif argdict.get('charttype')=="scatter":
+        scattermat(argdict,df);
+    elif argdict.get('charttype')=="count":
+        countmat(argdict,df)
+    elif argdict.get('charttype')=="line":
+        linemat(argdict,df);
+    elif argdict.get('charttype')=="hist":
+        histmat(argdict,df);
+    elif argdict.get('charttype')=="bar":
+        barmat(argdict,df);
+    else:
+        print("Error : Wrong Charttype")
+    plt.xlabel(argdict.get('x')) 
+    plt.ylabel(argdict.get('y'))
+    
+def scattersns(argdict):
+    x=input("Please provide x-axis column : ")
+    y=input("Please provide y-axis column : ")
+    marker=input("Please provide marker you want to use : ")
+    sns.scatterplot(data=argdict.get('data'),x=x,y=y,marker=marker);
+    
+def linesns(argdict):
+    x=input("Please provide the column on x-axis column : ")
+    y=input("Please provide the column on y-axis column : ")
+    marker=input("Please provide marker you want to use : ")
+    sns.lineplot(data=argdict.get('data'),x=x,y=y,marker=marker);
+    
+def heatsns(argdict):
+    anot=input("Please give t(true) or f(false) if you want annotation or not : ")
+    if anot=='t':anot=True;
+    elif anot=='f':anot=False;
+    else:print("Wrong Annotation");
+    sns.heatmap(data=argdict.get('data').corr(),annot=anot)
+    
+def countsns(argdict):
+    x=input("Please provide the column to be counted : ")
+    sns.countplot(data=argdict.get('data'),x=x)
+       
+def histsns(argdict):
+    x=input("Please provide the column on x-axis : ")
+    y=input("Please provide the column on y-axis : ")
+    argdict.update([('x',x),('y',y)])
+    if y=="":y=None
+    sns.histplot(data=argdict.get('data'),x=x,y=y)
 
-#######################################################################################
-df=pd.read_csv("E:\gyanesh\python\Dynamic Chart\Weather Data.csv")
-l=["Temp_C","Dew Point Temp_C","Rel Hum_%","Wind Speed_km/h","Visibility_km","Press_kPa"]
-plot(charttype="line",library="Seaborn",data=df,x="Temp_C",y="Speed_km/h",annot=True,vmin=-1,vmax=1);
+def barsns(argdict):
+    x=input("Please provide the column on x-axis : ")
+    y=input("Please provide the column on y-axis : ")
+    argdict.update([('x',x),('y',y)])
+    sns.barplot(data=argdict.get('data'),x=x,y=y)
+
+def piemat(argdict,df):
+    x=input("Please provide column you want to plot : ")
+    count=df[x].value_counts().reset_index()
+    autopct="%1.1f%%"
+    plt.pie(x=count[x],labels=count['index'],autopct=autopct)
+
+def countmat(argdict,df):
+    x=input("Please provide the column on x-axis : ")
+    countdf=df[x].value_counts().reset_index()
+    argdict.update([('x',x),('y','Count')])
+    plt.bar(countdf["index"],countdf["Weather"])
+
+def linemat(argdict,df):
+    x=input("Please provide the column on x-axis : ")
+    y=input("Please provide the column on y-axis : ")
+    marker=input("Please provide the marker you want to use : ")
+    x_values=list(df[x].unique())
+    x_values.sort()
+    y_values=[]
+    for i in x_values:
+        y_values.append(df.loc[df[x]==i,y].mean())
+    plt.plot(x_values,y_values,marker=marker)
+    argdict.update([('x',x),('y',y)])
+
+def histmat(argdict,df):
+    x=input("Please provide the column on x-axis : ")
+    plt.hist(df[x],edgecolor='k')
+    argdict.update([('x',x)])
+    
+def barmat(argdict,df):
+    x=input("Please provide the column on x-axis : ")
+    y=input("Please provide the column on y-axis : ")
+    plt.bar(df[x],height=df[y])
+    argdict.update([('x',x),('y',y)])
+
+def scattermat(argdict,df):
+    x=input("Please provide the column on x-axis : ")
+    y=input("Please provide the column on y-axis : ")
+    marker=input("Please provide marker you want to use : ")
+    plt.scatter(df[x],df[y],marker=marker)  
+    argdict.update([('x',x),('y',y)])
+
+def preinputs(argdict):
+    library=input("Please provide the library you want to use ['matplotlib'/'seaborn'] : ")
+    path=input("Please provide the path of dataset : ")
+    data=pd.read_csv(path)
+    argdict.update([('library',library),('data',data)])
+    
+    
+def postinputs(argdict):
+    title=input("Please provide the title : ")
+    rotation=input("Do you want to rotate labels [y/n] : ")
+    rotate_x=None;rotate_y=None;
+    if rotation=="y":
+        rotate_x=int(input("Provide rotation angle of x-labels : "))
+        rotate_y=int(input("Provide rotation angle of y-labels : "))
+    filename=input("Please provide the path where you want to save figure : ")
+    if filename=="":filename="C:/Users/rugyr/OneDrive/Pictures/fig.png";
+    argdict.update([('title',title),('rotate_y',rotate_y),('rotate_x',rotate_x),('savename',filename)])
+    
+dynamic_chart(argdict)
